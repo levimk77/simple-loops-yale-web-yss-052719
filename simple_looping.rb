f43e05e31775eaa761b016fd68652cb6f4ce45d5
Elijah Florence <efloren@siue.edu>
2019-04-17 16:03:11 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -38,6 +38,7 @@ def until_iterator(number_of_times)
   do until counter == number_of_times
   puts phrase
   counter += 1
+end
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
 end
